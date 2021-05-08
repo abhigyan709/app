8606412c3d94c2e6cfebd37df4a40db8e77b4b78
abhigyan <abhigyan709@gmail.com>
2021-05-09 00:22:23 +0530

    "Practice continues"

--- bin/app.dart
+++ bin/app.dart
@@ -1,4 +1,29 @@
 void main(List<String> arguments) {
   print('Hello world!');
   print('Hello Abhigyan');
+
+  var value = 18;
+  var myName = "Abhigyan";
+  print(value);
+  print(myName);
+
+  final name = "Gyanu"; //final variable can be used only once
+  print(name);
+  // now the name can be used again,
+  // name = "Gyan";
+  var a = 1;
+  var b = 1.0;
+
+  int x = 8;
+  double y = b + 6;
+  num z = 10 - y + x;
+  const valueA = 7;
+  const valueB = 2 * valueA;
+
+  print(a);
+  print(b);
+  print(x);
+  print(y);
+  print(z);
+  print(valueB);
 }
